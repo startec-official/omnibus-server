640d33aacb5543760ce166d0fdcb0f117cc48673
startec-official <startec.innovations@gmail.com>
2021-01-27 10:49:58 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const {Client} = require("@googlemaps/google-maps-services-js");
+const client = new Client({});
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+client
+  .elevation({
+    params: {
+      locations: [{ lat: 45, lng: -110 }],
+      key: process.env.API_KEY,
+    },
+    timeout: 1000, // milliseconds
+  })
+  .then((r) => {
+    console.log(r.data.results[0].elevation);
+  })
+  .catch((e) => {
+    console.log(e.response.data.error_message);
+  });
+
+console.log('hello world');
